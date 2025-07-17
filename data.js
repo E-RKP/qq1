4fe9907e4d521c7d317c35f1aee23eda105d8cbd
E-RKP <edobackup111@gmail.com>
2025-07-17 11:32:18 +0200

    version 3

--- data.js
+++ data.js
@@ -47,8 +47,41 @@ let lista = {
     for (i=0; i<this.utenti.length;i++){
       console.log(this.utenti[i].nickname)
     }
+  },
+
+  contautenti: function(){
+    let counter = 0
+    for (i=0; i<this.utenti.length;i++){
+      counter++;
+    }
+    return counter;
+  },
+
+  etàmedia: function(){
+    let counter =0
+    let somma = 0
+    for (i=0;i<this.utenti.length;i++){
+      somma = somma+this.utenti[i].età
+      counter++;
+    }
+
+    let media = somma/counter
+    return media;
+  },
+
+  meno25: function(){
+    let newArr = []
+    for (i=0; i<this.utenti.length;i++){
+      if (this.utenti[i].età < 25){
+        newArr.push(this.utenti[i])
+      }
+    }
+    return newArr;
   }
 }
 
 lista.stampanomi();
 lista.stampanickname();
+console.log(lista.contautenti())
+console.log(lista.etàmedia())
+console.log(lista.meno25())
