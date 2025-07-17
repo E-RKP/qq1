7147be89c4899c688de92d65af29d4aba023f2fd
E-RKP <edobackup111@gmail.com>
2025-07-17 11:05:36 +0200

    version 2

---
+++ data.js
@@ -0,0 +1,39 @@
+let lista = {
+
+  utenti: [
+  {
+    name: 'Paolo',
+    età: 22,
+    nickname: 'Gigi',
+    pa: '2u2943u9'
+  },
+
+  {
+    name: 'Mr mario',
+    età: 28,
+    nickname: 'loki',
+    pa: 'vevev'
+  },
+
+  {
+    name: 'maria',
+    età: 19,
+    nickname: 'Gig',
+    pa: 'ncwi/8j'
+  },
+
+  {
+    name: 'lucia',
+    età: 22,
+    nickname: 'wolf',
+    pa: '0987h'
+  },
+
+  {
+    name: 'Davide',
+    età: 33,
+    nickname: 'dedo',
+    pa: '2u2943umcwo,,9'
+  },
+  ]
+}
